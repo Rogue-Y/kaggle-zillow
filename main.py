7f00606c59cd4eab5d059d05dfd06c0637242577
hzn <benhzn07@gmail.com>
2017-07-18 22:26:00 -0400

    add config for models

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import feature_eng.utils as utils
 import feature_eng.data_clean as data_clean
 import feature_eng.feature_eng as feature_eng
 
-import json
+import evaluator
 
+import json
+import importlib # import module dynamically
+
+# Read config file
 steps_config_file = open("config/steps.json", "r")
-steps = None
+config = None
 try:
-    steps = json.load(steps_config_file)['steps']
+    config = json.load(steps_config_file)
 finally:
     steps_config_file.close()
 
-prop_df, df= None, None
+steps = config['steps']
+models = config['models']
 
+# Global variables
+prop_df, df= None, None
 
 print('Loading data ...')
 train, prop = utils.load_train_data()
@@ -64,4 +71,15 @@ X_train, y_train = utils.get_features_target(train_df)
 # 8562 rows
 X_test, y_test = utils.get_features_target(test_df)
 
-print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+# Evaluate
+ev = evaluator.Evaluator()
+ev.load_train_test((X_train, y_train, X_test, y_test))
+for model in models:
+    module_name = model['module']
+    module = importlib.import_module(module_name)
+    model_name = model['model']
+    model_to_use = getattr(module, model_name)
+    params = model['params']
+    ev.fit(model_to_use(**params))
+
+# print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
