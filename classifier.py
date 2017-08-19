7da459ceede79443728de93ce0197d9a9df41a42
hzn <benhzn07@gmail.com>
2017-08-18 21:19:35 -0400

    add resample

--- classifier.py
+++ classifier.py
@@ -1,13 +1,16 @@
 import feature_eng.utils as utils
 import feature_eng.data_clean as data_clean
 import feature_eng.feature_eng as feature_eng
-import evaluator
+from evaluator import Evaluator
 
+import math
 import numpy as np
 import pandas as pd
+import pickle
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
+import xgboost as xgb
 
 import os
 import gc
@@ -19,6 +22,9 @@ train_p = 'data/train_pickle'
 prop_p = 'data/prop_pickle'
 feature_eng_p = 'data/feature_eng_pickle'
 force_read = False
+
+xgboost_p = 'data/models/xgboost_pickle'
+force_train = True
 
 print('Loading data...')
 if not force_read and os.path.exists(train_p):
@@ -56,30 +62,79 @@ del train; gc.collect()
 train_df, validate_df = utils.split_by_date(df)
 train_df = data_clean.drop_training_only_column(train_df)
 validate_df = data_clean.drop_training_only_column(validate_df)
-print("Train data shape: ", train_df.shape)
+print("Train data shape before resample: ", train_df.shape)
+
+# Resample the training set
+train_df['logerror_cut'] = pd.cut(train_df['logerror'], 11, labels=False)
+resamples = []
+for i in range(11):
+    dist = pow(max(i - 5, 1), 5)
+    resample = train_df[train_df['logerror_cut'] == i].sample(
+        math.ceil(20000 / dist), replace = True, random_state = 42)
+    resamples.append(resample)
+train_df = pd.concat(resamples, copy=False)
+print("Train data shape after resample: ", train_df.shape)
+train_df.drop('logerror_cut', axis=1, inplace=True)
+print("Train data shape after resample: ", train_df.shape)
+
 #print("Outlier count: ", train_df['logerror'].sum())
 # Balance training sample
-print('Balancing...')
-# non_outliers = train_df[train_df['logerror'] == 0].sample(
-#     n=2000, replace=False, random_state=42)
-# train_df = pd.concat([train_df[train_df['logerror'] == 1], non_outliers])
-# print('Final training set shape: ', train_df.shape)
-outlier = train_df.nlargest(2000, 'logerror')
-outlier_bound = outlier['logerror'].min()
-print(outlier_bound)
-
-small = train_df.nsmallest(2000, 'logerror')
-small_bound = small.loc[0, 'logerror']
-print(outlier_bound)
+# print('Balancing...')
+# # non_outliers = train_df[train_df['logerror'] == 0].sample(
+# #     n=2000, replace=False, random_state=42)
+# # train_df = pd.concat([train_df[train_df['logerror'] == 1], non_outliers])
+# # print('Final training set shape: ', train_df.shape)
+# outlier = train_df.nlargest(2000, 'logerror')
+# outlier_bound = outlier['logerror'].min()
+# print(outlier_bound)
+#
+# small = train_df.nsmallest(2000, 'logerror')
+# small_bound = small.loc[0, 'logerror']
+# print(outlier_bound)
 
 X_train, y_train = utils.get_features_target(train_df)
 X_validate, y_validate = utils.get_features_target(validate_df)
 
 # Train classifier and predict
 print('Classify...')
-rf = RandomForestClassifier(n_estimators=100, random_state=42)
-rf.fit(X_train, y_train)
-y_pred = rf.predict(X_validate)
+# rf = RandomForestClassifier(n_estimators=100, random_state=42)
+# rf.fit(X_train, y_train)
+# y_pred = rf.predict(X_validate)
 
-print(classification_report(y_validate, y_pred))
-print("outlier prediction: ", y_pred.sum())
+# xgboost
+if not force_train and os.path.exists(xgboost_p):
+    with open(xgboost_p, 'rb') as xgboost_p_handler:
+        model = pickle.load(xgboost_p_handler)
+else:
+    print("\nSetting up data for XGBoost ...")
+    # xgboost params
+    xgb_params = {
+        'eta': 0.037,
+        'max_depth': 5,
+        'subsample': 0.80,
+        'objective': 'reg:linear',
+        'eval_metric': 'mae',
+        'lambda': 0.8,
+        'alpha': 0.4,
+        # 'base_score': y_mean,
+        'silent': 1
+    }
+
+    d_train = xgb.DMatrix(X_train, y_train)
+
+    num_boost_rounds = 250
+    print("num_boost_rounds="+str(num_boost_rounds))
+
+    # train model
+    print( "\nTraining XGBoost ...")
+    model = xgb.train(dict(xgb_params, silent=1), d_train, num_boost_round=num_boost_rounds)
+    with open(xgboost_p, 'wb') as xgboost_p_handler:
+        pickle.dump(model, xgboost_p_handler)
+
+print( "\nPredicting with XGBoost ...")
+d_validate = xgb.DMatrix(X_validate)
+y_pred = model.predict(d_validate)
+
+# print(classification_report(y_validate, y_pred))
+print("average mean error: ", Evaluator.mean_error(y_pred, y_validate))
+print("prediction summary: ", pd.Series(y_pred).describe())
