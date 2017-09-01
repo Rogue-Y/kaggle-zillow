51afb62959b16d824bb35c85b329d3427010910d
peterzjx <peterzjx@gmail.com>
2017-09-01 01:42:12 -0400

    rf var space

--- tune.py
+++ tune.py
@@ -8,21 +8,29 @@ configuration = test_config
 prop = prepare_features(configuration['feature_list'])
 
 # parameter space
+# space = {
+#     'model_params': {
+#         'learning_rate': hp.loguniform('learning_rate', -2, -1),
+#         'boosting_type': 'gbdt',
+#         'objective': 'regression',
+#         'metric': hp.choice('metric', ['mae', 'mse']),
+#         'sub_feature': hp.uniform('sub_feature', 0.03, 0.2),
+#         'num_leaves': hp.choice('num_leaves', list(range(50, 101, 10))),
+#         'min_data': hp.choice('min_data', list(range(200, 301, 10))),
+#         'min_hessian': hp.uniform('min_hessian', 0.3, 0.7),
+#         'num_boost_round': hp.choice('num_boost_round', [200, 300, 500]),
+#         'max_bin': hp.choice('max_bin', list(range(100, 301, 50))),
+#         'bagging_fraction': hp.uniform('bagging_fraction', 0.5, 1),
+#         'bagging_freq': hp.choice('bagging_freq', list(range(0, 100, 10))),
+#         'verbose': -1
+#     }
+# }
+
 space = {
     'model_params': {
-        'learning_rate': hp.loguniform('learning_rate', -2, -1),
-        'boosting_type': 'gbdt',
-        'objective': 'regression',
-        'metric': hp.choice('metric', ['mae', 'mse']),
-        'sub_feature': hp.uniform('sub_feature', 0.03, 0.2),
-        'num_leaves': hp.choice('num_leaves', list(range(50, 101, 10))),
-        'min_data': hp.choice('min_data', list(range(200, 301, 10))),
-        'min_hessian': hp.uniform('min_hessian', 0.3, 0.7),
-        'num_boost_round': hp.choice('num_boost_round', [200, 300, 500]),
-        'max_bin': hp.choice('max_bin', list(range(100, 301, 50))),
-        'bagging_fraction': hp.uniform('bagging_fraction', 0.5, 1),
-        'bagging_freq': hp.choice('bagging_freq', list(range(0, 100, 10))),
-        'verbose': -1
+        'max_features': hp.uniform('max_features', 0.1, 0.5),
+        'min_samples_leaf': hp.choice('min_samples_leaf', list(range(50, 101, 10))),
+        'n_estimators': hp.choice('n_estimators', list(range(50, 151, 10))),
     }
 }
 
